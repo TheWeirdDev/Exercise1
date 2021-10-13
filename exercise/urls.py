13837d889419f4ce429ca3ae7bf81325b5d0532d
Alireza6677 <alireza6677@gmail.com>
2021-10-14 00:00:35 +0330

    Specified urls for the app and django auth views

--- exercise/urls.py
+++ exercise/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('', include('show_management.urls')),
 ]
